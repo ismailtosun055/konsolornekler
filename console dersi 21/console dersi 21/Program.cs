456bf30387448f9d26f5d794afb1b51840c7b98c
ismailtosun055 <ismailtosunn.0202@gmail.com>
2024-06-04 14:15:17 +0300

    dosyalari yukluyorum

---
+++ console dersi 21/console dersi 21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace console_dersi_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer ses = new SoundPlayer();
+            string yol;
+            yol = "C:\\Users\\ismai\\Music\\asktan_da_ote_seksenler_enstrumantal_album_aac_34239.wav";
+            ses.SoundLocation= yol;
+            ses.Play();
+            Console.ReadLine();
+            //System.Diagnostics.Process.Start("C:\\Users\\ismai\\Music\\asktan_da_ote_seksenler_enstrumantal_album_aac_34239.m4a");
+        }
+    }
+}
---
+++ console dersi 22/console dersi 22/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_dersi_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] dizi = new int[5,5];
+            int[,] matris= new int[3,3];
+            int i, k;
+            Console.WriteLine("lütfen matrisinizi oluşturun:");
+            for(i=0;i<5;i++)
+            {
+                for(k=0;k<5;k++)
+                {
+                    dizi[i,k]=Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("lütfen matrisinizi oluşturun:");
+            for (i = 0; i < 5; i++)
+            {
+                for (k = 0; k < 5; k++)
+                {
+                    matris[i, k] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+
+        }
+    }
+}
